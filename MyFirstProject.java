2dba5ff4e5934aa8def60a7d6211ee1d2259831a
José Isaack <joseisaack05@gmail.com>
2025-10-02 14:42:51 -0400

    Create MyFirstProject.java

---
+++ MyFirstProject.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        /* 🎲Tipos primitivos de dados🎲
+         São tipos de valores armazenados diretamente na memória (stacks ou stackado)
+         */
+
+        int age = 19;
+        int year = 2025;
+        int quantity = 1;
+
+        double price = 40.99;
+        double gpa = 4.5;
+        double tempeture = -12.3;
+
+        char grade = 'A';
+        char symbol = '?';
+        char br_currency = 'R$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        System.out.println("My name is ");
+
+        }
+    }
+}
