fc3b49fd9e2306829ab4b05746c82fb6a099a18a
José Isaack <joseisaack05@gmail.com>
2025-10-10 15:51:57 -0400

    Create Programade_CarrinhodeCompra.java

---
+++ Programade_CarrinhodeCompra.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String item;
+        String moeda = "R$";
+        double preco;
+        int quantidade;
+        double total;
+
+        System.out.print("Que item você gostaria de comprar?: ");
+        item = scanner.nextLine();
+
+        System.out.print("Qual é o preço por cada?: ");
+        preco = scanner.nextDouble();
+
+        System.out.print("Quantos você gostaria?: ");
+        quantidade = scanner.nextInt();
+
+        total = preco * quantidade;
+
+        System.out.println("\nVocê comprou " + quantidade + " " + item +"/s");
+        System.out.println("Seu total é " + moeda + total);
+
+        scanner.close();
+    }
+}
