2f0a78666a0797d76bf1fab77f1a2a04dae59b19
José Isaack <joseisaack05@gmail.com>
2025-10-16 17:08:49 -0400

    Update CalculadoraDE_JurosCompostos.java

--- CalculadoraDE_JurosCompostos.java
+++ CalculadoraDE_JurosCompostos.java
@@ -1,1 +1,37 @@
+import java.util.Scanner;
 
+public class main {
+    public static void main(String[] args) {
+
+        // calculadora de juros compostos
+
+        Scanner scanner = new Scanner(System.in);
+
+        double capitalInicial;
+        double taxa;
+        int vezesComposto;
+        int anos;
+        double valor;
+
+        System.out.print("Digite o valor inicial: ");
+        capitalInicial = scanner.nextDouble();
+
+        System.out.print("Digite a taxa de juros (em %): ");
+        taxa = scanner.nextDouble() / 100;
+
+        System.out.print("Digite o número de vezes compostos por ano: ");
+        vezesComposto = scanner.nextInt();
+
+        System.out.print("Digite o número do ano: ");
+        anos = scanner.nextInt();
+
+        valor = capitalInicial * Math.pow(1 + taxa / vezesComposto, vezesComposto * anos);
+
+        System.out.printf("O valor após %d ano(s) é R$%.2f", anos, valor);
+
+        // System.out.printf("O valor depois de %d anos é R$ %,f ", anos, valor );
+
+        scanner.close();
+
+    }
+}
