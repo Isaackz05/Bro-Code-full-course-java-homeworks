b11b8aa7ced1e5ecd75a52689e5eaf6077c3095d
José Isaack <joseisaack05@gmail.com>
2025-10-21 16:57:58 -0400

    Update MatematicaEmProgramacao.java

--- MatematicaEmProgramacao.java
+++ MatematicaEmProgramacao.java
@@ -6,11 +6,19 @@ public class main {
 
         double resultado;
 
+        /* ------------------------------------------------------------------------------------------------------------- */
+
+        // POTÊNCIA
+
         resultado = Math.pow(2, 5);
         /*
         esse metodo e para saber a potencia de ambos os numeros
         tipo 2³ (dois ao cubo)
         */
+
+        /* ------------------------------------------------------------------------------------------------------------- */
+
+        // VALOR ABSOLUTO
 
         resultado = Math.abs(-5);
         /*
@@ -21,8 +29,16 @@ public class main {
         se o numero ja for positivo ele não muda nada, continua o mesmo
         */
 
+        /* ------------------------------------------------------------------------------------------------------------- */
+
+        // RAIZ QUADRADA
+
         resultado = Math.sqrt(9);
         // metodo para mostrar a raiz quadrada de um numero
+
+        /* ------------------------------------------------------------------------------------------------------------- */
+
+        // ARRENDONDAR
 
         resultado  = Math.round(3.15);
         /*
@@ -34,18 +50,32 @@ public class main {
         14 vira 10
         */
 
+        /* ------------------------------------------------------------------------------------------------------------- */
+
+        // ARRENDONDAR PARA CIMA
+
         resultado = Math.ceil(3.12);
         //  arredonda o número para CIMA (para o proximo número inteiro maior)
         //  ceil vem de "ceiling" que significa "teto" em ingles
 
+        /* ------------------------------------------------------------------------------------------------------------- */
+
+        // ARRENDONDAR PARA BAIXO
+
         resultado = Math.floor(3.99);
         // faz a mesma coisa que o ceil só que arredonda para baixo (para o número anterior)
         // floor significa "chão" em ingles
+
+        /* ------------------------------------------------------------------------------------------------------------- */
+
+        // ACHAR O MAIOR E/OU MENOR VALOR
 
         resultado = Math.max(10, 20);
         resultado = Math.min(10, 20);
         // vão achar o máximo e o mínimo entre os valores X e Y.
 
         System.out.println(resultado);
+
+        /* ------------------------------------------------------------------------------------------------------------- */
     }
 }
