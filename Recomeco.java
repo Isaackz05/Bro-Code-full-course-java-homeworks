1b1fd98652fdfa396cb6cf5d434e388184e81b7f
Isaack Hugueney <joseisaack05@gmail.com>
2025-11-18 16:28:19 -0400

    Create Recomeco.java

---
+++ Recomeco.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        // Comentarios, meu primeiro programa em java depois de alguns dias sem praticar
+
+        System.out.print("Eu gosto de pizza\n");
+        System.out.print("É muito bom\n");
+        System.out.print("Compra para mim!\n");
+    }
+}
